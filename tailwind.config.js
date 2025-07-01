5e5f25942f10e65467180c1dcdbb5e180eaaf21a
smartburme <athuyrain@gmail.com>
2025-07-01 14:11:43 +0630

    Create tailwind.config.js
    Signed-off-by: smartburme <athuyrain@gmail.com>

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1e293b',
+        secondary: '#334155',
+        accent: '#f472b6',
+      },
+    },
+  },
+  plugins: [],
+}
